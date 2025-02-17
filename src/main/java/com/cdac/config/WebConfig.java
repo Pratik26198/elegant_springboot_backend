1d04381f15f977ca5a1e0396309b6f4a1f044aa7
pratik26198 <pratik260198@gmail.com>
2025-02-17 15:14:06 +0530

    connectivity

--- src/main/java/com/cdac/config/WebConfig.java
+++ src/main/java/com/cdac/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Frontend URL
+                        .allowedOrigins("https://elegantjewellary.vercel.app/", "http://localhost:3000")  // Add frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
